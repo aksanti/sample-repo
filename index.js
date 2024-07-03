cb22911187f4bcd32c651bbd541e47497af19faa
aksanti <aksantibamba@hotmail.com>
2024-07-03 19:25:43 +0200

    New code

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function dispayGreeting(name) {
-    const message = `Hello, ${name}!`;
-    console.log(message);
+// Greeting message function
+function createGreetingMessage(name) {
+  const message = `Hello, ${name}`;
+  return message;
 }
 
-dispayGreeting('Koumba')
+// const greetingMessage = createGreetingMessage('Christopher');
+// const greetingMessage = createGreetingMessage('Christopher');
+
+function displayDone() {
+    console.log('3 second has elapsed');
+}
+
+setTimeout(displayDone, 3000);
+
+
+setTimeout(function () {
+    console.log('4 second has elapsed');
+}, 4000);
+
+setTimeout(() => { console.log('5 second has elapsed'); }, 5000);
