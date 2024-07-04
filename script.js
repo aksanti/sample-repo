92635f302107268d5c005b66b7f3b19856b113b1
aksanti <aksantibamba@hotmail.com>
2024-07-04 17:07:41 +0200

    This firs commit script file

---
+++ script.js
@@ -0,0 +1,7 @@
+let currentMoney = 0, laptopPrice = 1;
+
+if (currentMoney >= laptopPrice) {
+    console.log("Getting a new laptop!");
+} else {
+    console.log("Can't affrod a new laptop, yet!");
+}
